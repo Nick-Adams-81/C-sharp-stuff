4bf6788de921c78b226d4b662ba0e5074e786878
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-12 15:19:16 -0600

    basic functionaity done

--- CSharpPracticeStuff/Program.cs
+++ CSharpPracticeStuff/Program.cs
@@ -16,8 +16,35 @@ namespace CSharpPracticeStuff
             Console.WriteLine("{0}: Version: {2} By: {1}", appName, appAuthor, appVersion);
             Console.ResetColor();
 
-            Console.WriteLine("test");
 
+            Console.WriteLine("What is your name?");
+            string nameResponse = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets play a game", nameResponse);
+
+            int correctNumber = 7;
+            int guess = 0;
+
+            Console.WriteLine("Guess a number from 1 and 10");
+
+            while (guess != correctNumber)
+            {
+                string playerGuess = Console.ReadLine();
+                guess = Int32.Parse(playerGuess);
+
+                if (guess != correctNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Guess again!!");
+                    Console.ResetColor();
+                }
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("You win!!!");
+            Console.ResetColor();
+                
 
         }
     }
