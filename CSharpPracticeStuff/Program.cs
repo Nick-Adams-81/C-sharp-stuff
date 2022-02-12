fc837dc75512de9fe2cbc53870e3e1863af7fa43
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-12 15:36:16 -0600

    fully functioning game pre refactor

--- CSharpPracticeStuff/Program.cs
+++ CSharpPracticeStuff/Program.cs
@@ -22,29 +22,62 @@ namespace CSharpPracticeStuff
 
             Console.WriteLine("Hello {0}, lets play a game", nameResponse);
 
-            int correctNumber = 7;
-            int guess = 0;
 
-            Console.WriteLine("Guess a number from 1 and 10");
 
-            while (guess != correctNumber)
+            while (true)
             {
-                string playerGuess = Console.ReadLine();
-                guess = Int32.Parse(playerGuess);
 
-                if (guess != correctNumber)
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+                int guess = 0;
+
+                Console.WriteLine("Guess a number from 1 and 10");
+                while (guess != correctNumber)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Guess again!!");
-                    Console.ResetColor();
+                    string playerGuess = Console.ReadLine();
+
+                    if (!int.TryParse(playerGuess, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please guess a number");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    guess = Int32.Parse(playerGuess);
+
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong number, Guess again!!");
+                        Console.ResetColor();
+                    }
+
+                }
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("You win!!!");
+                Console.ResetColor();
+
+                Console.WriteLine("Play again[Y/N]?");
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else if(answer == "N")
+                {
+                    Console.WriteLine("Thanks for playing!!!");
+                    return;
+                }
+                else
+                {
+                    return;
                 }
 
             }
 
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("You win!!!");
-            Console.ResetColor();
-                
 
         }
     }
